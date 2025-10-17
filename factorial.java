e088daed8261689b7b7ab76c65321b39137f934d
automationArijit <watercolour.arijit@gmail.com>
2025-10-17 21:44:00 +0530

    modify

--- factorial.java
+++ factorial.java
@@ -5,12 +5,12 @@ public class factorial {
 	public static void main(String[] args) {
 				
 		int fact=1;
-		for(int i=1;i<=5;i++)
+		for(int i=1;i<=6;i++)
 		{
 			fact=fact*i;
 		}
 		System.out.println("The factorial of five is > "+fact);
-
+               
 		
 		
 
